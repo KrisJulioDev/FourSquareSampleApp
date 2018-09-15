783360ac969c95dac546fb0927eee0b53a5f5ec9
Khristoffer Julio <krisgamedev@gmail.com>
2018-09-15 20:22:40 +0800

    Initial Commit

---
+++ FourSquareSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FourSquareSampleApp
+//
+//  Created by Khristoffer Julio on 15/09/2018.
+//  Copyright © 2018 Khristoffer Julio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
